f8fa7bae173973bb4555131b88aed10297c22db4
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 19:02:36 +0530

    Added: flip a coin multiple times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
 
-echo "FLIP COIN SIMULATOR"
-
-if [ $((RANDOM%2)) -eq 0 ]
-then
-	echo "HEADS Won"
-else
-	echo "TAILS Won"
-fi
+h=0
+t=0
+read -p "how many times should coin flip enter a number: " flip
+a=0
+while [[ $a -lt $flip ]] 
+do
+  a=$((a+1))
+  fliped=$((RANDOM%2))
+	if [ $fliped -eq 0 ]
+	then
+			echo "HEADS"
+			h=$((h+1))
+	else
+			echo "TAILS"
+			t=$((t+1))
+	fi
+done
+echo "no of times heads won: " $h
+echo "no of times tails won: " $t
