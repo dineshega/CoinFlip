50741463c91674c0b83727ab5f5596fc3d9417bb
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 19:09:00 +0530

    Added: if its tie, game continues till the difference of minimum of 2 points is achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -16,17 +16,20 @@ do
 			echo "TAILS"
 			t=$((t+1))
 	fi
-	if [ $h -eq 21 ]
+	if [[ $h -eq 21 || $((h-t)) -gt 2 ]]
 	then
 		echo "Heads Won"
 		h=$((h-t))
 		echo "by how much: " $h
 		a=$flip
-	elif [ $t -eq 21 ]
+	elif [[ $t -eq 21 || $((t-h)) -gt 2 ]]
 	then
 		echo "Tails Won"
 		t=$((t-h))
 		echo "by how much: " $t
 		a=$flip
+	elif [ $h -eq $t ]
+	then
+		echo "$h=$t its TIE"
 	fi
-done
+done	
